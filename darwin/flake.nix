6a39ce57379fa17a83a2f85d861351b61b93943e
bahrom04 <magdiyevbahrom@gmail.com>
2025-05-25 07:29:12 +0500

    pzds

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -1,98 +1,44 @@
+# ~/nix-config/flake.nix
 {
-  description = "bahrom04 system flake";
+  description = "My macOS config with nix-darwin + Home Manager";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
-    nix-darwin.url = "github:nix-darwin/nix-darwin/master";
+    nix-darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs =
-    inputs@{
-      self,
-      nix-darwin,
-      home-manager,
-      nixpkgs,
-    }:
+  outputs = { self, nixpkgs, nix-darwin, home-manager }:
     let
-      configuration =
-        { pkgs, ... }:
-        {
-          # List packages installed in system profile. To search by name, run:
-          # $ nix-env -qaP | grep wget
-          environment.systemPackages = with pkgs; [
-            neovim
-            fastfetch
-          ];
-
-          # Fonts
-          fonts.packages = [
-            # deprecated (24.11)
-            # (pkgs.nerdfonts.override {
-            #   fonts = [
-            #     "Iosevka"
-            #     "IosevkaTerm"
-            #   ];
-            # })
-            pkgs.nerd-fonts.iosevka
-            pkgs.nerd-fonts.victor-mono
-          ];
-
-          # Necessary for using flakes on this system.
-          nix.settings.experimental-features = "nix-command flakes";
-
-          security.pam.services.sudo_local.touchIdAuth = true;
-
-          # Enable alternative shell support in nix-darwin.
-          programs.fish.enable = true;
-
-          system.primaryUser = "bahrom04";
-
-          # Set Fish as the default shell
-          users.knownUsers = [ "bahrom04" ];
-          users.users = {
-            bahrom04 = {
+      system = "aarch64-darwin";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      darwinConfigurations.bahrom04 = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          {
+            nix.settings.experimental-features = "nix-command flakes";
+            environment.systemPackages = with pkgs; [ neovim fastfetch ];
+            programs.fish.enable = true;
+            users.users.bahrom04 = {
+              name = "bahrom04";
+              home = "/Users/bahrom04";
               shell = pkgs.fish;
               uid = 501;
-              home = "/Users/bahrom04";
             };
-          };
-
-          # Set Git commit hash for darwin-version.
-          system.configurationRevision = self.rev or self.dirtyRev or null;
-
-          # Used for backwards compatibility, please read the changelog before changing.
-          # $ darwin-rebuild changelog
-          system.stateVersion = 5;
-
-          # The platform the configuration will be used on.
-          nixpkgs.hostPlatform = "aarch64-darwin";
-        };
-    in
-    {
-
-      # Reusable darwin modules you might want to export
-      # These are usually stuff you would upstream into nixpkgs
-      darwinModules = import ./modules/darwin;
-
-      # Build darwin flake using:
-      # $ darwin-rebuild build --flake .#bahrom04
-      darwinConfigurations."bahrom04" = nix-darwin.lib.darwinSystem {
-        modules = [
-          configuration
+            system.stateVersion = 5;
+          }
           home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
             home-manager.users.bahrom04 = import ./home.nix;
           }
-          self.darwinModules.homebrew
         ];
       };
     };
--- darwin/home.nix
+++ darwin/home.nix
@@ -2,14 +2,14 @@
 
 {
   home.username = "bahrom04";
-
+  home.homeDirectory = "/Users/bahrom04";
 
   programs.home-manager.enable = true;
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
-    initExtra = ''
+    initContent = ''
       eval "$(starship init zsh)"
     '';
   };
@@ -27,6 +27,40 @@
     enable = true;
     nix-direnv.enable = true;
   };
+
+  # Foydalanuvchi profiliga oʻrnatilishi kerak boʻlgan paketlar
+  home.packages = with pkgs; [
+    # ushbu keltirilganlar men kunlik davomida ishlatadiganlarim
+    # o'zingiznikini qo'shish yoki keltirilganlardan olib tashashdan tortinmang
+    
+    neofetch
+    
+    # arxivlar
+    zip
+    xz
+    unzip
+    
+    # utilitalar
+    jq # json ustida ishlovchi yengil va qulay instrument    
+    # tarmoq utilitalari
+    mtr # tarmoq diagnostika utilitasi
+    dnsutils # `dig` + `nslookup`
+    ldns # `dig` o'rnini bosuvchi, `drill` buyrug'ini taqdim etadi
+    aria2 # multi-protokol va multi-manba'lik buyruq satridagi yuklovchi utilita
+    nmap # tarmoq qidiruvchi va xavfsizlik auditi uchun utilita
+ 
+    
+    # hokazo
+    cowsay
+    file
+    which
+    tree
+    # nix ga tegishli
+    # 
+    # `nom` buyrug'ini beradi va `nix` day ishlaydi
+    # tafsilotli log chiqishini ta’minlaydi
+    # nix-output-monitor
+  ];
 
   home.stateVersion = "25.05";
 
