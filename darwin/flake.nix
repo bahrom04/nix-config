465ee35a915fc96133af2d742096fd067110af37
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-05-28 23:06:26 +0500

    Update flake.nix

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -3,15 +3,21 @@
   description = "My macOS config with nix-darwin + Home Manager";
 
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+
+    # Nixpkgs for darwin
     nix-darwin.url = "github:lnl7/nix-darwin/nix-darwin-25.05";
+
+    # Theme version as Nixpkgs
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
 
+    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
+      };
     };
-  };
 
   outputs = { self, nixpkgs, nix-darwin, home-manager }:
     let
@@ -24,39 +30,40 @@
           {
             nix.settings.experimental-features = "nix-command flakes";
             environment.systemPackages = with pkgs; [ 
-              neovim 
-              fastfetch 
-              redis	
-              ];
+		          neovim 
+		          fastfetch 
+		          redis	
+		        ];
 
-	          services.redis.enable = true;
-	    
-            programs.fish.enable = true;
-	    
-	          system.defaults.dock = {
-              autohide = false;
-              largesize = 16;
-              mineffect = "scale";
-              minimize-to-application = true;
-              mru-spaces = true;
-              orientation = "bottom";
-              show-recents = false;
-              show-process-indicators = true;
-              tilesize = 50;
+	      services.redis.enable = true;
+
+
+        programs.fish.enable = true;
+
+        # MacOs Dock setttings
+	      system.defaults.dock = {
+		    autohide = false;
+		    largesize = 16;
+	      mineffect = "scale";
+		    minimize-to-application = true;
+		    mru-spaces = true;
+		    orientation = "bottom";
+		    show-recents = false;
+		    show-process-indicators = true;
+		    tilesize = 50;
+			  };
+
+	      system.primaryUser = "bahrom04";
+          users.users.bahrom04 = {
+            name = "bahrom04";
+            home = "/Users/bahrom04";
+            shell = pkgs.fish;
+            uid = 501;
             };
-
-	          system.primaryUser = "bahrom04";
-
-            users.users.bahrom04 = {
-              name = "bahrom04";
-              home = "/Users/bahrom04";
-              shell = pkgs.fish;
-              uid = 501;
-            };
-
             system.stateVersion = 5;
           }
-          home-manager.darwinModules.home-manager
+
+        home-manager.darwinModules.home-manager
           {
             home-manager.useGlobalPkgs = true;
             home-manager.useUserPackages = true;
