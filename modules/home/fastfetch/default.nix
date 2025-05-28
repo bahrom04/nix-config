c547cf4ca3ab70d9c1b94e71d84449a73b39cbb5
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-05-28 08:11:07 +0500

    Update default.nix

--- modules/home/fastfetch/default.nix
+++ modules/home/fastfetch/default.nix
@@ -4,34 +4,16 @@
     programs.fastfetch = {
         enable = true;
         settings = {
-            logo = {
-                source = "nixos_small";
-                padding = {
-                right = 1;
-                };
-            };
-            display = {
-                size = {
-                binaryPrefix = "si";
-                };
-                color = "blue";
-                separator = "  ";
-            };
             modules = [
                 {
                 type = "datetime";
                 key = "Date";
                 format = "{1}-{3}-{11}";
                 }
-                {
-                type = "datetime";
-                key = "Time";
-                format = "{14}:{17}:{20}";
-                }
                 "title"
                 "os"
                 "host"
-                "karnel"
+                "kernel"
                 "uptime"
                 "packages"
                 "shell"
@@ -45,6 +27,7 @@
                 "swap"
                 "disk"
                 "localip"
+                "PublicIp"
                 "battery"
                 "locale"
             ];
