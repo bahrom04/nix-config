92072d3c776d4a5e12513be22f539cf94a39f77c
bahrom04 <116780481+bahrom04@users.noreply.github.com>
2025-05-28 23:06:02 +0500

    Update home.nix add aria2c

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,16 +1,29 @@
 { config, pkgs, ... }:
 
 {
+  imports = [
+     ./modules/fastfetch/default.nix
+     ];
   home.username = "bahrom04";
   home.homeDirectory = "/Users/bahrom04";
 
   programs.home-manager.enable = true;
+  # home-manager.backupFileExtension = "backup";
 
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+    autosuggestion.enable = true;
     initContent = ''
       eval "$(starship init zsh)"
+    '';
+  };
+
+  programs.fish = {
+    enable = true;
+    generateCompletions = true;
+    loginShellInit = ''
+      eval "$(starship init fish)"
     '';
   };
 
@@ -27,7 +40,8 @@
     enable = true;
     nix-direnv.enable = true;
   };
-
+  
+  
   # Foydalanuvchi profiliga oʻrnatilishi kerak boʻlgan paketlar
   home.packages = with pkgs; [
     # ushbu keltirilganlar men kunlik davomida ishlatadiganlarim
@@ -46,10 +60,11 @@
     mtr # tarmoq diagnostika utilitasi
     dnsutils # `dig` + `nslookup`
     ldns # `dig` o'rnini bosuvchi, `drill` buyrug'ini taqdim etadi
-    aria2 # multi-protokol va multi-manba'lik buyruq satridagi yuklovchi utilita
     nmap # tarmoq qidiruvchi va xavfsizlik auditi uchun utilita
- 
-    
+
+    # aria2c -x 16 -s 16  "https://CONTENT_PATH"
+    aria2 # multi-protokol va multi-manba'lik buyruq satridagi yuklovchi utilita
+
     # hokazo
     cowsay
     file
@@ -58,9 +73,35 @@
     # nix ga tegishli
     # 
     # `nom` buyrug'ini beradi va `nix` day ishlaydi
-    # tafsilotli log chiqishini ta’minlaydi
+    # tafsilotli log chiqishini ta’minlaydi  
     # nix-output-monitor
+  
   ];
+
+  home.file.".aria2/aria2.conf".text = ''
+    dir=/Users/${config.home.username}/Downloads
+    continue=true
+    max-connection-per-server=5
+    split=5
+    daemon=true
+    enable-rpc=true
+    rpc-listen-port=6800
+    rpc-allow-origin-all=true
+  '';
+
+  launchd.agents.aria2 = {
+    enable = true;
+    config = {
+      ProgramArguments = [
+        "${pkgs.aria2}/bin/aria2c"
+        "--conf-path=$HOME/.aria2/aria2.conf"
+      ];
+      RunAtLoad = true;
+      KeepAlive = true;
+      StandartOutPath = "$HOME/Library/Logs/aria2.logs";
+      StandartErrorPath = "$HOME/Library/Logs/aria2.err";
+    };
+  };
 
   home.stateVersion = "25.05";
 
