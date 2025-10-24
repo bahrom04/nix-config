c35228ed20ffe3647b984cba3c6d5ecbd2cc1121
bahrom04 <magdiyevbahrom@gmail.com>
2025-10-24 20:02:32 +0500

    chore: update layout

--- modules/desktop.nix
+++ modules/desktop.nix
@@ -12,13 +12,13 @@
       extraLayouts.uz = {
         description = "Uzbek (Oʻzbekiston)";
         languages = ["eng" "uzb"];
-        symbolsFile = "${pkgs.fetchFromGitHub {
-          owner = "bahrom04";
-          repo = "uzbek-latin-keyboard";
-          rev = "main";
-          hash = "sha256-29UHkTc+k+IXt491zKows2Y4g1Ba6XdjlO2wnTMJFQM=";
-        }}/uz";
-        # symbolsFile = ../uz;
+        # symbolsFile = "${pkgs.fetchFromGitHub {
+        #   owner = "bahrom04";
+        #   repo = "uzbek-latin-keyboard";
+        #   rev = "main";
+        #   hash = "sha256-29UHkTc+k+IXt491zKows2Y4g1Ba6XdjlO2wnTMJFQM=";
+        # }}/uz";
+        symbolsFile = ../uz;
       };
       layout = "uz,us";
       variant = "latin";
