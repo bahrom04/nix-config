3d79f74624a114196f2d4f5c33b4c7a0d2c74f97
bahrom04 <magdiyevbahrom@gmail.com>
2025-11-25 18:43:53 +0500

    chore: update zed config

--- modules/home/zed.nix
+++ modules/home/zed.nix
@@ -1,161 +1,3 @@
-# {pkgs, ...}: let
-#   extensions = [
-#     "glsl"
-#     "haskell"
-#     "html"
-#     "ini"
-#     "just"
-#     "latex"
-#     "lua"
-#     "make"
-#     "material-icon-theme"
-#     "vscode-icons"
-#     "neocmake"
-#     "nginx"
-#     "nix"
-#     "nu"
-#     "pkl"
-#     "slint"
-#     "sql"
-#     "toml"
-#     "typst"
-#     "vercel-theme"
-#     "wgsl"
-#     "xml"
-#     "zig"
-
-#   ];
-
-#   settings = {
-#     auto_update = true;
-
-#     disable_ai = true;
-
-#     telemetry = {
-#       metrics = false;
-#       diagnostics = false;
-#     };
-
-#     show_edit_predictions = true;
-
-#     languages = {
-#       Markdown = {
-#         format_on_save = "on";
-#         use_on_type_format = true;
-#         remove_trailing_whitespace_on_save = true;
-#       };
-
-#       Nix = {
-#         formatter = "language_server";
-#         language_servers = [
-#           "nixd"
-#           "!nil"
-#         ];
-#       };
-#     };
-
-#     lsp = {
-#       nixd = {
-#         binary = {
-#           ignore_system_version = false;
-#         };
-#         settings = {
-#           formatting = {
-#             command = [
-#               "alejandra"
-#             ];
-#           };
-#           diagnostic = {
-#             suppress = [
-#               "sema-extra-with"
-#               "sema-extra-rec"
-#             ];
-#           };
-#         };
-#       };
-
-#       rust-analyzer = {
-#         binary = {
-#           ignore_system_version = false;
-#         };
-#         initialization_options = {
-#           check = {
-#             command = "clippy";
-#           };
-#         };
-#       };
-
-#       solargraph = {
-#         binary = {
-#           ignore_system_version = false;
-#         };
-#         initialization_options = {
-#           diagnostics = true;
-#           formatting = true;
-#         };
-#       };
-#     };
-
-#     load_direnv = "shell_hook";
-
-#     theme = {
-#       mode = "system";
-#       light = "Ayu Light";
-#       dark = "VSCode Dark Modern";
-#     };
-#     icon_theme = "VSCode Icons for Zed (Dark)";
-
-#     tab_size = 2;
-#     preferred_line_length = 100;
-
-#     autosave = "on_focus_change";
-#     formatter = "on";
-#     enable_language_server = true;
-
-#     soft_wrap = "editor_width";
-
-#     buffer_font_size = 16;
-#     # buffer_font_family = "mononoki";
-
-#     ui_font_size = 16;
-#     # ui_font_family = "mononoki";
-
-#     confirm_quit = false;
-#     use_autoclose = false;
-
-#     inlay_hints = {
-#       enabled = true;
-#       # show_background = true;
-#     };
-
-#     title_bar = {
-#       show_branch_icon = true;
-#     };
-
-#     collaboration_panel = {
-#       button = false;
-#     };
-
-#     # chat_panel = {
-#     #   button = "never";
-#     # };
-
-#     agent = {
-#       enabled = false;
-#     };
-#   };
-# in {
-#   config = {
-#     programs.zed-editor = {
-#       enable = true;
-#       inherit extensions;
-#       userSettings = settings;
-#       installRemoteServer = true;
-#       extraPackages = with pkgs; [nixd];
-#     };
-#   };
-# }
-# -------------------------------------------------------------------
 {
   lib,
   pkgs,
@@ -211,7 +53,7 @@
 
     languages = {
       Markdown = {
-        format_on_save = "on";
+        formatter = "auto";
         use_on_type_format = true;
         remove_trailing_whitespace_on_save = true;
       };
@@ -306,7 +148,8 @@
     preferred_line_length = 100;
 
     autosave = "off";
-    format_on_save = "language_server";
+    formatter = "auto";
+
     enable_language_server = true;
 
     soft_wrap = "editor_width";
