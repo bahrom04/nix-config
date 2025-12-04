08ca49a410d91080cfe56ff69fd0ee16b1bad82e
bahrom04 <magdiyevbahrom@gmail.com>
2025-12-04 19:02:02 +0500

    chore: enable nix-ld

--- hosts/matax/configuration.nix
+++ hosts/matax/configuration.nix
@@ -47,8 +47,10 @@
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
   system.stateVersion = "25.11";
+  programs.nix-ld.enable = true;
   environment.systemPackages = [
     pkgs.sqlitebrowser
     pkgs.boltbrowser
+    pkgs.kstars
   ];
 }
