ffcced3f67f17a23e78a558009011df35ed3700c
bahrom04 <magdiyevbahrom@gmail.com>
2026-01-15 23:44:11 +0500

    chore: some changes

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -36,14 +36,16 @@
     enable = true;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
     settings = {
+      # download-buffer-size = 524288000; # 500 MiB to prevent buffer warnings
+      
       experimental-features = "nix-command flakes pipe-operators";
       substituters = [
-        "https://cache.nixos.org/"
         "https://cache.xinux.uz/"
+        "https://cache.nixos.org/"
       ];
       trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
       trusted-users = [
         "root"
