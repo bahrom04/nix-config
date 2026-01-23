99f28dcd97b229df80cd224428ff0215a918ca77
bahrom04 <magdiyevbahrom@gmail.com>
2026-01-23 17:54:22 +0500

    chore: still testing .nar not found thing

--- modules/nixpkgs.nix
+++ modules/nixpkgs.nix
@@ -40,12 +40,12 @@
 
       experimental-features = "nix-command flakes pipe-operators";
       substituters = [
-        "https://cache.xinux.uz/"
         "https://cache.nixos.org/"
+        "https://cache.xinux.uz/"
       ];
       trusted-public-keys = [
-        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
       ];
       trusted-users = [
         "root"
--- modules/remote-builder.nix
+++ modules/remote-builder.nix
@@ -6,6 +6,7 @@
         sshUser = "bahrom04";
         system = "x86_64-linux";
         protocol = "ssh-ng";
+        sshKey = "~/.ssh/id_ed25519";
         maxJobs = 4;
         speedFactor = 2;
         supportedFeatures = [
