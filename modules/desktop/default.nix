cb522726faf104dcafb7b445f79e0f118cfb03e7
bahrom04 <magdiyevbahrom@gmail.com>
2026-02-04 10:15:59 +0500

    chore: add keybindings

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -114,7 +114,7 @@ in {
 
           [org.gnome.shell.extensions.dash-to-dock]
           click-action='minimize'
-          
+
           # Don't hibernate on delay
           [org.gnome.settings-daemon.plugins.power]
           sleep-inactive-ac-type='nothing'
@@ -122,6 +122,15 @@ in {
           # Don't sleep, don't sleep!
           [org.gnome.desktop.session]
           idle-delay=0
+
+          [org.gnome.settings-daemon.plugins.media-keys]
+          custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+          screensaver=['<Control><Alt>l']
+
+          [org.gnome.settings-daemon.plugins.media-keys.custom-keybindings.custom0]
+          binding='<Shift><Control>t'
+          command='kgx --tab'
+          name='open terminal'
         '';
         extraGSettingsOverridePackages = [
           pkgs.gsettings-desktop-schemas
