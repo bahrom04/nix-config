67543cf28fc8b0697eccad9d87f494461336dc24
bahrom04 <magdiyevbahrom@gmail.com>
2026-02-22 19:02:09 +0500

    chore: add hs packages to home

--- modules/home/packages/default.nix
+++ modules/home/packages/default.nix
@@ -1,33 +1,35 @@
 {pkgs, ...}: {
-  # Foydalanuvchi profiliga oʻrnatilishi kerak boʻlgan paketlar
   home.packages = with pkgs; [
     git
     gnupg # gpg key uchun
     neofetch
     curl
-    # arxivlar
     zip
     xz
     unzip
-    jq # json ustida ishlovchi yengil va qulay instrument
-    mtr # tarmoq diagnostika utilitasi
-    dnsutils # `dig` + `nslookup`
-    ldns # `dig` o'rnini bosuvchi, `drill` buyrug'ini taqdim etadi
-    nmap # tarmoq qidiruvchi va xavfsizlik auditi uchun utilita
-
-    # aria2c -x 16 -s 16  "https://CONTENT_PATH"
-    aria2 # multi-protokol va multi-manba'lik buyruq satridagi yuklovchi utilita
-
-    # hokazo
+    jq
+    mtr
+    dnsutils
+    ldns
+    nmap
     cowsay
     file
     which
     tree
-    # nix-output-monitor
     btop
-
-    # l10n
+    htop
     wl-clipboard
     #espanso-wayland
+
+    # aria2c -x 16 -s 16  "https://CONTENT_PATH"
+    aria2
+
+    # wth
+    haskell.packages."ghc910".haskell-language-server
+    haskell.packages."ghc910".ghc
+    haskell.packages."ghc910".cabal-install
+    haskell.packages."ghc910".haskell-language-server
+    haskell.packages."ghc910".cabal-fmt
+    haskell.packages."ghc910".fourmolu
   ];
 }
