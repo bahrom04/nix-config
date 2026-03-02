8d115887074f1a89735488fc166121801859ffe7
bahrom04 <magdiyevbahrom@gmail.com>
2026-03-02 20:01:25 +0500

    chore: temorarly disable desktop icon extension til it get fixed

--- modules/nixos/extension/default.nix
+++ modules/nixos/extension/default.nix
@@ -9,7 +9,8 @@
       pkgs.gnomeExtensions.dash-to-dock
       pkgs.gnomeExtensions.gsconnect
       pkgs.gnomeExtensions.clipboard-indicator
-      pkgs.gnomeExtensions.gtk4-desktop-icons-ng-ding
+      # pkgs.gnomeExtensions.gtk4-desktop-icons-ng-ding
+      # pkgs.gjs
       # icons
       pkgs.papirus-icon-theme
     ];
