c603470af418c1b718aa8432875bc26ecd67f25e
bahrom04 <magdiyevbahrom@gmail.com>
2026-03-02 19:18:21 +0500

    chore: desktop icon extension

--- modules/nixos/extension/default.nix
+++ modules/nixos/extension/default.nix
@@ -9,6 +9,7 @@
       pkgs.gnomeExtensions.dash-to-dock
       pkgs.gnomeExtensions.gsconnect
       pkgs.gnomeExtensions.clipboard-indicator
+      pkgs.gnomeExtensions.gtk4-desktop-icons-ng-ding
       # icons
       pkgs.papirus-icon-theme
     ];
