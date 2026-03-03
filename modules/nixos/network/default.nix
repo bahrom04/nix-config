897e0e059e128a211234e42a3b841302711487d1
bahrom04 <magdiyevbahrom@gmail.com>
2026-03-03 12:16:49 +0500

    chore: enable bluetooth

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -1,22 +1,22 @@
 { ... }:
 {
-  config = {
-    # Enable networking
-    networking = {
-      networkmanager.enable = true;
-      # hostName = "matax"; # Define your hostname.
+  services.blueman.enable = true;
+  hardware.bluetooth.enable = true;
+  # Enable networking
+  networking = {
+    networkmanager.enable = true;
+    # hostName = "matax"; # Define your hostname.
 
-      firewall = {
-        allowedTCPPorts = [
-          22
-          80
-          443
-        ];
-        allowedUDPPorts = [
-          80
-          443
-        ];
-      };
+    firewall = {
+      allowedTCPPorts = [
+        22
+        80
+        443
+      ];
+      allowedUDPPorts = [
+        80
+        443
+      ];
     };
   };
 }
