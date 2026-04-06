f6f746db9548e2b58b421824dbe209eb4fc08a8e
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-06 11:52:02 +0500

    refactor: disable podman and remove some trash

--- modules/nixos/pkgs/default.nix
+++ modules/nixos/pkgs/default.nix
@@ -11,115 +11,121 @@ let
 in
 {
   # APPS
-  environment.systemPackages = with pkgs; [
-    gnome-tweaks
-    gnome-extension-manager
-    gnomeExtensions.appindicator
-    gnomeExtensions.dash-to-dock
-    gnomeExtensions.clipboard-indicator
-    papirus-icon-theme
-    # formatter
-    nixfmt-tree
-    nixfmt
-    nixpkgs-review
-    # fonts
-    freefont_ttf
-    gnome-user-share
-    desktop-file-utils
-    neovim
-    vim
-    fastfetch
-    onefetch
-    xkbcomp
-    setxkbmap
-    rng-tools
-    haveged
-    flatpak-builder
-    woeusb # for creating windows usb
-    ntfs3g
+  environment = {
+    variables = {
+      EDITOR = "vim";
+      SOPS_AGE_KEY_FILE = age_keys;
+    };
+    systemPackages = with pkgs; [
+      gnome-tweaks
+      gnome-extension-manager
+      gnomeExtensions.appindicator
+      gnomeExtensions.dash-to-dock
+      gnomeExtensions.clipboard-indicator
+      papirus-icon-theme
+      # formatter
+      nixfmt-tree
+      nixfmt
+      nixpkgs-review
+      # fonts
+      freefont_ttf
+      gnome-user-share
+      desktop-file-utils
+      neovim
+      vim
+      fastfetch
+      onefetch
+      xkbcomp
+      setxkbmap
+      rng-tools
+      haveged
+      flatpak-builder
+      woeusb # for creating windows usb
+      ntfs3g
 
-    fuse # version 2 for appimages
-    appimage-run
+      fuse # version 2 for appimages
+      appimage-run
 
-    # git/hub
-    age
-    sops
-    pinentry-gnome3
+      # git/hub
+      age
+      sops
+      pinentry-gnome3
 
-    # l10n
-    hunspell
-    hunspellDicts.uz_UZ
-    # Services
-    # redis
-    # support both 32-bit and 64-bit applications
-    wineWow64Packages.stable
-    samba # Provides ntlm_auth
-    krb5 # Provides Kerberos support libraries
-    winetricks # Useful for further troubleshooting
-    gnome-builder
-    fractal
-    # authenticator
-    libreoffice
-    gnome-boxes
-    # prismlauncher
-    # gparted
-    googleearth-pro
-    onlyoffice-desktopeditors
-    # postman
-    gtranslator
-    google-chrome
-    brave
-    # chromium
-    github-desktop
-    element-desktop
-    telegram-desktop
-    discord
-    # kstars
-    # gnome-solanum
-    nextcloud-client
-    icon-library
-    gnome-podcasts
-    # cambalache
-    bustle
-    d-spy
-    firefox
-    gradia
-    dialect
-    cpu-x
-    sqlitebrowser
-    blanket
-    cisco-packet-tracer_9
-    # autopsy
-    e-imzo-manager
-    software-center
-    xinux-module-manager
-    xinux-settings
-    xeonitte
-    obsidian
-    crosswords
-    (poedit.override { boost = boost188; })
-    # cosmic-settings
-    # cosmic-settings-daemon
-    burpsuite
-    zap
-    seclists
-    # veracrypt
-    # cryptomator
-    # kdePackages.kleopatra
-    # hashcat
-    wireshark
-    sysprof
-    # constrict
-    # winboat
-    # pods
-    # podman
-    # opensnitch-ui
-    # virtualbox
-    # vmware-workstation
-    # tailor_gui
-    # openscad-lsp
-    # openscad
-  ];
+      # l10n
+      hunspell
+      hunspellDicts.uz_UZ
+      # Services
+      # redis
+      # support both 32-bit and 64-bit applications
+      wineWow64Packages.stable
+      samba # Provides ntlm_auth
+      krb5 # Provides Kerberos support libraries
+      winetricks # Useful for further troubleshooting
+      gnome-builder
+      fractal
+      # authenticator
+      libreoffice
+      gnome-boxes
+      # prismlauncher
+      # gparted
+      googleearth-pro
+      onlyoffice-desktopeditors
+      # postman
+      gtranslator
+      google-chrome
+      brave
+      # chromium
+      github-desktop
+      element-desktop
+      telegram-desktop
+      discord
+      # kstars
+      # gnome-solanum
+      nextcloud-client
+      icon-library
+      gnome-podcasts
+      # cambalache
+      bustle
+      d-spy
+      firefox
+      gradia
+      dialect
+      cpu-x
+      sqlitebrowser
+      blanket
+      cisco-packet-tracer_9
+      # autopsy
+      e-imzo-manager
+      software-center
+      xinux-module-manager
+      xinux-settings
+      xeonitte
+      obsidian
+      crosswords
+      (poedit.override { boost = boost188; })
+      # cosmic-settings
+      # cosmic-settings-daemon
+      burpsuite
+      zap
+      seclists
+      # veracrypt
+      # cryptomator
+      # kdePackages.kleopatra
+      # hashcat
+      wireshark
+      sysprof
+      # constrict
+      # winboat
+      # pods
+      # podman
+      # opensnitch-ui
+      # virtualbox
+      # vmware-workstation
+      # tailor_gui
+      # openscad-lsp
+      # openscad
+    ];
+  };
 
   # advansed option. Adding just pkgs.x is not enough
   programs = {
@@ -151,12 +157,6 @@ in
           pkgs.fuse # Provides FUSE 2 support
           pkgs.libpulseaudio # Often needed for sound in AppImages
         ];
-      };
-    };
-    environment = {
-      variables = {
-        EDITOR = "vim";
-        SOPS_AGE_KEY_FILE = age_keys;
       };
     };
     nix-ld = {
