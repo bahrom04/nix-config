ee05be2364e46180a453c963c82d8df83ff37565
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-15 15:52:51 +0500

    chore: add more famous fonts

--- modules/nixos/fonts/default.nix
+++ modules/nixos/fonts/default.nix
@@ -1,19 +1,24 @@
 { pkgs, ... }:
 {
-  fonts.packages = with pkgs; [
-    # An alternative popular Chinese font
-    wqy_zenhei
+  fonts = {
+    fontDir.enable = true;
+    packages = with pkgs; [
+      # An alternative popular Chinese font
+      wqy_zenhei
 
-    corefonts
-    vista-fonts-chs
-    noto-fonts
-    noto-fonts-color-emoji
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
-    nerd-fonts.jetbrains-mono
-    mplus-outline-fonts.osdnRelease
-    liberation_ttf
-    fira-code
-    fira-code-symbols
-  ];
+      corefonts
+      carlito
+      vista-fonts
+      vista-fonts-chs
+      noto-fonts
+      noto-fonts-color-emoji
+      noto-fonts-cjk-sans
+      noto-fonts-cjk-serif
+      nerd-fonts.jetbrains-mono
+      mplus-outline-fonts.osdnRelease
+      liberation_ttf
+      fira-code
+      fira-code-symbols
+    ];
+  };
 }
