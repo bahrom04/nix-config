c92d374dc3d97d543cc28c1570812009bddbd766
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-15 14:04:26 +0500

    chore: added more fonts

--- modules/nixos/fonts/default.nix
+++ modules/nixos/fonts/default.nix
@@ -1,18 +1,19 @@
 { pkgs, ... }:
 {
   fonts.packages = with pkgs; [
-    corefonts
-    vista-fonts-chs
-    # General Noto fonts
-    noto-fonts
-    noto-fonts-color-emoji
-    # CJK specific fonts (Sans and Serif)
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
     # An alternative popular Chinese font
     wqy_zenhei
-    # Other potential CJK fonts
-    mplus-outline-fonts.osdnRelease
 
+    corefonts
+    vista-fonts-chs
+    noto-fonts
+    noto-fonts-color-emoji
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    nerd-fonts.jetbrains-mono
+    mplus-outline-fonts.osdnRelease
+    liberation_ttf
+    fira-code
+    fira-code-symbols
   ];
 }
