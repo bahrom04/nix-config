690584122ab1f3115cfcecb2056594dd3b459694
bahrom04 <magdiyevbahrom@gmail.com>
2026-04-15 15:53:08 +0500

    chore: more fonts

--- modules/nixos/fonts/default.nix
+++ modules/nixos/fonts/default.nix
@@ -1,24 +1,41 @@
 { pkgs, ... }:
 {
   fonts = {
+    enableDefaultPackages = true;
     fontDir.enable = true;
     packages = with pkgs; [
       # An alternative popular Chinese font
       wqy_zenhei
-
+      ubuntu-classic
       corefonts
       carlito
       vista-fonts
       vista-fonts-chs
+      font-bh-ttf
       noto-fonts
-      noto-fonts-color-emoji
       noto-fonts-cjk-sans
       noto-fonts-cjk-serif
       nerd-fonts.jetbrains-mono
       mplus-outline-fonts.osdnRelease
-      liberation_ttf
       fira-code
       fira-code-symbols
+      hermit
+      source-code-pro
+      terminus_font
+      font-awesome
+      font-awesome_4
+      hack-font
+      noto-fonts
+      cantarell-fonts
+      powerline-fonts
+      roboto
+      roboto-slab
+      eb-garamond
+      fira-code
+      fira-code-symbols
+      mplus-outline-fonts.githubRelease
+      dina-font
+      proggyfonts
     ];
   };
 }
