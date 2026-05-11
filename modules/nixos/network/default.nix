2f36fe36d74bf2baddcab7c1e98592954cdd667f
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-11 15:16:36 +0500

    add: addguard and nextdns

--- modules/nixos/network/default.nix
+++ modules/nixos/network/default.nix
@@ -2,6 +2,18 @@
 {
   # Enable networking
   networking = {
+    nameservers = [
+      # AdGuard DNS
+      "94.140.14.14"
+      "94.140.15.15"
+      "2a10:50c0::ad1:ff"
+      "2a10:50c0::ad2:ff"
+      # NextDNS
+      "45.90.28.148"
+      "45.90.30.148"
+      "2a07:a8c0::35:958c"
+      "2a07:a8c1::35:958c"
+    ];
     networkmanager.enable = true;
     # hostName = "matax"; # Define your hostname.
 
