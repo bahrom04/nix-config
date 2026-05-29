f37ff440d2e00d716078e42f9688b226beeb988e
bahrom04 <magdiyevbahrom@gmail.com>
2026-05-29 21:03:11 +0500

    chore: enable module-manager

--- systems/x86_64-linux/dell/modules.nix
+++ systems/x86_64-linux/dell/modules.nix
@@ -3,5 +3,5 @@
   modules.xinux.eimzoIntegraion.enable = true;
   services.flatpak.enable = true;
   modules.xinux.browser = "firefox";
-  modules.xinux.xinuxModuleManager.enable = false;
+  modules.xinux.xinuxModuleManager.enable = true;
 }
