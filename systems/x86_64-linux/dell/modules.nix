9d094df7901e30599a972be8db919389eb33f2c5
bahrom04 <magdiyevbahrom@gmail.com>
2026-06-01 03:40:30 +0500

    chore(dell): enable rusted tools

--- systems/x86_64-linux/dell/modules.nix
+++ systems/x86_64-linux/dell/modules.nix
@@ -1,6 +1,7 @@
 { ... }:
 {
   modules.xinux.eimzoIntegraion.enable = true;
+  modules.shell.rusted-tools = true;
   services.flatpak.enable = true;
   modules.xinux.browser = "firefox";
   modules.xinux.xinuxModuleManager.enable = true;
