f9e49a9bbbd6b9a5c5871341cc51fd4495f3ed03
bahrom04 <magdiyevbahrom@gmail.com>
2026-06-28 13:49:20 +0500

    remove nix settings cache keys as favour of xinux modules handles it

--- modules/nixos/nixpkgs/default.nix
+++ modules/nixos/nixpkgs/default.nix
@@ -16,16 +16,16 @@
 
     settings = {
       # download-buffer-size = 524288000; # 500 MiB to prevent buffer warnings
-      substituters = [
-        "https://cache.xinux.uz?priority=10"
-        # "https://attic.xuyh0120.win/lantian" # cachy
-        # "https://nix-community.cachix.org"
-      ];
-      trusted-public-keys = lib.mkBefore [
-        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
-        # "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
-        # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
+      # substituters = [
+      #   # "https://cache.xinux.uz?priority=10"
+      #   # "https://attic.xuyh0120.win/lantian" # cachy
+      #   # "https://nix-community.cachix.org"
+      # ];
+      # trusted-public-keys = lib.mkBefore [
+      #   "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
+      #   # "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+      #   # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      # ];
       trusted-users = [
         "@wheel"
       ];
