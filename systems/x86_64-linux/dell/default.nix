7d18727cbcf28a6ed930895d0d4f36e4610e414b
bahrom04 <magdiyevbahrom@gmail.com>
2026-07-20 00:08:05 +0500

    open terminal shortcut with dconf

--- systems/x86_64-linux/dell/default.nix
+++ systems/x86_64-linux/dell/default.nix
@@ -18,6 +18,27 @@
   #       return polkit.Result.YES;
   #   });
   # '';
+  programs = {
+    dconf = {
+      enable = true;
+      profiles.user.databases = [
+        {
+          settings = {
+            "org/gnome/settings-daemon/plugins/media-keys" = {
+              custom-keybindings = [
+                "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+              ];
+            };
+            "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+              name = "open-terminal";
+              binding = "<Shift><Control>t";
+              command = "kgx";
+            };
+          };
+        }
+      ];
+    };
+  };
 
   services = {
     system76-scheduler = {
