7ff15e5e7fa90f5d8648dc401fe06cdd424a33c2
bahrom04 <magdiyevbahrom@gmail.com>
2026-07-22 22:25:32 +0500

    add shortcuts to dell

--- systems/x86_64-linux/dell/default.nix
+++ systems/x86_64-linux/dell/default.nix
@@ -27,12 +27,22 @@
             "org/gnome/settings-daemon/plugins/media-keys" = {
               custom-keybindings = [
                 "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+                "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
               ];
+
             };
             "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
               name = "open-terminal";
-              binding = "<Shift><Control>t";
+              binding = "<Ctrl><Alt>t";
               command = "kgx";
+            };
+            "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+              name = "open-nautilus";
+              binding = "<Super>e";
+              command = "nautilus";
+            };
+            "org/gnome/desktop/wm/preferences" = {
+              resize-with-right-button = true;
             };
           };
         }
