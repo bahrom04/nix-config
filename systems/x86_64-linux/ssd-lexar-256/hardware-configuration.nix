386a01649e438744e78a855607f7ccf9f91ad18a
bahrom04 <magdiyevbahrom@gmail.com>
2026-07-25 23:59:46 +0500

    add test ssd config host

---
+++ systems/x86_64-linux/ssd-lexar-256/hardware-configuration.nix
@@ -0,0 +1,79 @@
+{
+  inputs,
+  lib,
+  modulesPath,
+  pkgs,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+    inputs.disko.nixosModules.disko
+    ./disk-configuration.nix
+  ];
+
+  hardware.facter = {
+    enable = true;
+    reportPath = ./dell.json;
+    detected.graphics.enable = true;
+  };
+
+  boot = {
+    zswap = {
+      enable = true;
+    };
+    kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v3;
+    initrd.systemd.enable = true;
+    kernelModules = [ "fuse" ];
+    kernelParams = [
+      "intel_pstate=active"
+      "mem_sleep_default=deep"
+    ];
+    supportedFilesystems = [ "ntfs" ];
+    # kernel.sysctl = {
+    #   "net.core.default_qdisc" = "fq";
+    #   "net.ipv4.tcp_congestion_control" = "bbr";
+    # };
+  };
+
+  # Hardware optimized compilation
+  # https://nixos.wiki/wiki/Build_flags
+  nix.settings.system-features = lib.systems.architectures.features.skylake;
+  nixpkgs.hostPlatform = {
+    gcc.arch = "skylake";
+    gcc.tune = "skylake";
+    system = "x86_64-linux";
+  };
+  nixpkgs.buildPlatform = {
+    gcc.arch = "skylake";
+    gcc.tune = "skylake";
+    system = "x86_64-linux";
+  };
+  # nixpkgs.localSystem = {
+  #   gcc.arch = "skylake";
+  #   gcc.tune = "skylake";
+  #   system = "x86_64-linux";
+  # };
+  # nixpkgs.hostPlatform = "x86_64-linux";
+
+  # List packages system hardware configuration
+  # CPU (Intel/Ryzen) luchshe kupi ryzen: https://www.youtube.com/watch?v=GOkm2C0rk-w
+  hardware = {
+    graphics = {
+      enable = true;
+      enable32Bit = true;
+      extraPackages = with pkgs; [
+        intel-media-driver # For Broadwell (2014) or newer processors (includes 10th Gen)
+        intel-vaapi-driver # Optional, for older applications
+        vpl-gpu-rt # For modern QSV
+        libvdpau
+        libva-vdpau-driver
+        # vulkan-validation-layers
+      ];
+    };
+    cpu.intel.updateMicrocode = true;
+  };
+  environment.sessionVariables = {
+    LIBVA_DRIVER_NAME = "iHD";
+  };
+}
