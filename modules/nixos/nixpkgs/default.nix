6d15c7765b8f5ca6ea06ea6fa16d3c883b0b1e8c
bahrom04 <magdiyevbahrom@gmail.com>
2026-08-29 14:57:19 +0500

    sax

--- modules/nixos/nixpkgs/default.nix
+++ modules/nixos/nixpkgs/default.nix
@@ -21,6 +21,7 @@
       ];
       # download-buffer-size = 524288000; # 500 MiB to prevent buffer warnings
       substituters = [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://cache.xinux.uz?priority=10"
         # "https://attic.xuyh0120.win/lantian" # cachy
         # "https://nyx-cache.chaotic.cx/"
@@ -28,7 +29,6 @@
       ];
       trusted-public-keys = [
         "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
-        "https://mirrors.ustc.edu.cn/nix-channels/store"
         # "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
         #   # "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         #   # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
