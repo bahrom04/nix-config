02721105c28be1c16e7492bad96aa3a4cb410d01
bahrom04 <magdiyevbahrom@gmail.com>
2026-08-29 13:01:47 +0500

    xinux cache

--- modules/nixos/nixpkgs/default.nix
+++ modules/nixos/nixpkgs/default.nix
@@ -21,15 +21,15 @@
       ];
       # download-buffer-size = 524288000; # 500 MiB to prevent buffer warnings
       substituters = [
+        "https://cache.xinux.uz?priority=10"
         "https://mirrors.ustc.edu.cn/nix-channels/store"
-        # "https://cache.xinux.uz?priority=10"
         # "https://attic.xuyh0120.win/lantian" # cachy
         # "https://nyx-cache.chaotic.cx/"
         # "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
         "https://mirrors.ustc.edu.cn/nix-channels/store"
-        #   "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
         # "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
         #   # "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         #   # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
