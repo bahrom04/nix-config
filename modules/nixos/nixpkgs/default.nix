546f0b4f1a78ca30192ac7d32fd63878cc63bd88
bahrom04 <magdiyevbahrom@gmail.com>
2026-08-29 12:55:52 +0500

    china mirror

--- modules/nixos/nixpkgs/default.nix
+++ modules/nixos/nixpkgs/default.nix
@@ -20,15 +20,17 @@
         "recursive-nix"
       ];
       # download-buffer-size = 524288000; # 500 MiB to prevent buffer warnings
-      substituters = lib.mkAfter [
+      substituters = [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
         # "https://cache.xinux.uz?priority=10"
         # "https://attic.xuyh0120.win/lantian" # cachy
-        "https://nyx-cache.chaotic.cx/"
+        # "https://nyx-cache.chaotic.cx/"
         # "https://nix-community.cachix.org"
       ];
-      trusted-public-keys = lib.mkAfter [
+      trusted-public-keys = [
+        "https://mirrors.ustc.edu.cn/nix-channels/store"
         #   "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0=" # xinux
-        "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
+        # "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
         #   # "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         #   # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
