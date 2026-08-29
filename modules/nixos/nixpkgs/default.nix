7706bfe669f832e72a6e62f0c9a53c4f86c18e21
bahrom04 <magdiyevbahrom@gmail.com>
2026-08-29 15:25:32 +0500

    lock update

--- modules/nixos/nixpkgs/default.nix
+++ modules/nixos/nixpkgs/default.nix
@@ -8,7 +8,6 @@
     enable = true;
     nixPath = lib.mapAttrsToList (key: value: "${key}=${value.to.path}") config.nix.registry;
 
-    # Garbage collector.
     gc = {
       automatic = true;
       options = "--delete-older-than 10d";
@@ -21,13 +20,15 @@
       ];
       # download-buffer-size = 524288000; # 500 MiB to prevent buffer warnings
       substituters = [
-        "https://mirrors.ustc.edu.cn/nix-channels/store"
+        "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+        # "https://mirrors.ustc.edu.cn/nix-channels/store"
         "https://cache.xinux.uz?priority=10"
         # "https://attic.xuyh0120.win/lantian" # cachy
         # "https://nyx-cache.chaotic.cx/"
         # "https://nix-community.cachix.org"
       ];
       trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cache.xinux.uz:BXCrtqejFjWzWEB9YuGB7X2MV4ttBur1N8BkwQRdH+0="
         # "nyx-cache.chaotic.cx:dJxTrgMC3V3cFfyIiBQDQorG6k1LsqurH/srpMSq7qk="
         #   # "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
