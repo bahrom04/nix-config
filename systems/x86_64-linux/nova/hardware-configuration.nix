70109c6b3f5de3a9fe98dcabe2e8b37d1550f179
bahrom04 <magdiyevbahrom@gmail.com>
2026-09-11 19:28:55 +0500

    enable finegrained nvidia

--- systems/x86_64-linux/nova/hardware-configuration.nix
+++ systems/x86_64-linux/nova/hardware-configuration.nix
@@ -25,54 +25,25 @@
     };
     kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest-x86_64-v3;
     initrd.systemd.enable = true;
-    kernelModules = [ "fuse" ];
+    kernelModules = [
+      "fuse"
+      "nvidia"
+    ];
     kernelParams = [
       "intel_pstate=active"
       "mem_sleep_default=deep"
     ];
     supportedFilesystems = [ "ntfs" ];
-    # kernel.sysctl = {
-    #   "net.core.default_qdisc" = "fq";
-    #   "net.ipv4.tcp_congestion_control" = "bbr";
-    # };
   };
 
-  # Hardware optimized compilation
-  # https://nixos.wiki/wiki/Build_flags
-  # nix.settings.system-features = lib.systems.architectures.features.skylake;
   nix.settings.system-features = [
-    # "gccarch-skylake"
     "gccarch-x86-64-v3"
   ];
-  # nixpkgs.hostPlatform = lib.mkForce {
-  #   gcc.arch = "skylake";
-  #   gcc.tune = "skylake";
-  #   system = "x86_64-linux";
-  # };
-  # nixpkgs.hostPlatform = lib.mkForce {
-  #   system = "x86_64-linux";
-  #   gcc.arch = "skylake";
-  #   gcc.tune = "skylake";
-  #   aesSupport = true;
-  #   avxSupport = true;
-  #   avx2Support = true;
-  #   sse3Support = true;
-  #   ssse3Support = true;
-  #   sse4_1Support = true;
-  #   sse4_2Support = true;
-  # };
   nixpkgs.buildPlatform = lib.mkForce {
     gcc.arch = "x86-64-v3";
     gcc.tune = "x86-64-v3";
     system = "x86_64-linux";
   };
-  # nixpkgs.localSystem = {
-  #   gcc.arch = "skylake";
-  #   gcc.tune = "skylake";
-  #   system = "x86_64-linux";
-  # };
-  # nixpkgs.hostPlatform = "x86_64-linux";
-
   services.xserver.videoDrivers = [
     "nvidia"
     "modesetting"
@@ -82,6 +53,7 @@
     nvidia = {
       open = true;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
+      powerManagement.finegrained = true;
       prime = {
         intelBusId = "PCI:0@0:2:0";
         nvidiaBusId = "PCI:1@0:0:0";
@@ -93,9 +65,9 @@
       enable = true;
       enable32Bit = true;
       extraPackages = with pkgs; [
-        intel-media-driver # For Broadwell (2014) or newer processors (includes 10th Gen)
-        intel-vaapi-driver # Optional, for older applications
-        vpl-gpu-rt # For modern QSV
+        intel-media-driver
+        intel-vaapi-driver
+        vpl-gpu-rt
         libvdpau
         libva-vdpau-driver
         # vulkan-validation-layers
